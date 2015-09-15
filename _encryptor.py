af021f4980975944c16e9fbf307c6b231c6b20d9
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-09-15 14:43:56 +0200

    encrypt dummy passwords

---
+++ _encryptor.py
@@ -0,0 +1,11 @@
+__author__ = 'Samuel Gratzl'
+
+
+if __name__ == '__main__':
+  import uuid, hashlib
+  password = raw_input('enter password: ')
+  salt = uuid.uuid4().hex
+  hashed_password = hashlib.sha512(password + salt).hexdigest()
+  print password
+  print salt
+  print hashed_password
--- dummy_store.py
+++ dummy_store.py
@@ -2,11 +2,17 @@ __author__ = 'Samuel Gratzl'
 
 import caleydo_security_flask.flask_login
 
+import hashlib
+
+def hash_password(password, salt):
+  return hashlib.sha512(password + salt).hexdigest()
+
 class User(caleydo_security_flask.flask_login.User):
-  def __init__(self, id, password, roles):
+  def __init__(self, id, password, salt, roles):
     super(User, self).__init__(id)
     self.name = id
     self._password = password
+    self._salt = salt
     self.roles = roles
 
   def is_authenticated(self):
@@ -15,11 +21,15 @@ class User(caleydo_security_flask.flask_login.User):
   def is_active(self):
     return True
 
+  def is_password(self, given):
+    given_h = hash_password(given, self._salt)
+    return given_h == self._password
+
 class UserStore(object):
   def __init__(self):
     import caleydo_server.config
 
-    self._users = [User(v['name'], v['password'], v['roles']) for v in
+    self._users = [User(v['name'], v['password'], v['salt'], v['roles']) for v in
                    caleydo_server.config.get('caleydo_security_flask.users')]
 
   def load(self, id):
@@ -29,10 +39,10 @@ class UserStore(object):
     parts = api_key.split(':')
     if len(parts) != 2:
       return None
-    return next((u for u in self._users if u.id == parts[0] and u._password == parts[1]), None)
+    return next((u for u in self._users if u.id == parts[0] and u.is_password(parts[1])), None)
 
   def login(self, username, extra_fields = {}):
-    return next((u for u in self._users if u.id == username and u._password == extra_fields['password']), None)
+    return next((u for u in self._users if u.id == username and u.is_password(extra_fields['password'])), None)
 
   def logout(self, user):
     pass
