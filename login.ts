333b5f0db2bfc791bf947673c6f97586fbbf678c
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-02-10 12:03:37 +0100

    inline login form dialog

--- login.ts
+++ login.ts
@@ -1,8 +1,14 @@
 /**
  *
  */
+/// <amd-dependency path="text!./_login_form.html" name="formTemplate"/>
 import ajax = require('../caleydo_core/ajax');
+import C = require('../caleydo_core/main');
 import session = require('../caleydo_core/session');
+
+declare var formTemplate:string;
+
+export const form = formTemplate;
 
 export function login(username:string, password:string, remember = false) {
   return ajax.send('/login', {
@@ -20,19 +26,26 @@ export function login(username:string, password:string, remember = false) {
   });
 }
 
-export function logout() {
-  return ajax.send('/logout', {}, 'post').then(function (user) {
-    session.remove('user');
-    session.remove('user_obj');
-  });
+export function logout() : Promise<any> {
+  if (!C.offline) {
+    return ajax.send('/logout', {}, 'post').then(function (user) {
+      session.remove('user');
+      session.remove('user_obj');
+    });
+  }
+  session.remove('user');
+  session.remove('user_obj');
+  return Promise.resolve(true);
 }
 
 export function bindLoginForm(form: HTMLFormElement, callback: (error, user) => any) {
-  ajax.getJSON('/loggedinas')
-    .then((user) => callback(null, user))
-    .catch((error) => {
-      //ignore not yet logged in
-    });
+  if (!C.offline) {
+    ajax.getJSON('/loggedinas')
+      .then((user) => callback(null, user))
+      .catch((error) => {
+        //ignore not yet logged in
+      });
+  }
   form.onsubmit = (event) => {
 
     const username = (<any>form).login_username.value;
