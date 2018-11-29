a333a5c15ffd2b775ad4d854c7accd32b898d2fb
Holger Stitz <holger.stitz@jku.at>
2018-11-29 15:04:23 +0100

    Add encryptor_python3.py compatible with Python 3

---
+++ encryptor_python3.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+__author__ = 'Samuel Gratzl'
+
+if __name__ == '__main__':
+  import uuid
+  import hashlib
+
+  password = input('enter password: ').encode('utf-8')
+  salt = uuid.uuid4().hex.encode('utf-8')
+  hashed_password = hashlib.sha512(password + salt).hexdigest()
+  print(password)
+  print(salt)
+  print(hashed_password)
