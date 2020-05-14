e4f8fbe67e054a40e4fe86aa6aad94811d0efa11
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-14 12:33:18 +0200

    add index.ts file

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './extensions';
+export * from './login';
+export * from './LoginMenu';
+export {default as LoginMenu} from './LoginMenu';
+export {default} from './phovea';
+export * from './watcher';
+export {default as startWatching} from './watcher';
