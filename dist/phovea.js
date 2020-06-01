eb874175360ba17ab711e68713dac730d33c679d
anita-steiner <k9908600@gmx.at>
2020-06-01 13:20:37 +0200

    change registry entries

--- dist/phovea.js
+++ dist/phovea.js
@@ -2,9 +2,9 @@ import { PluginRegistry } from 'phovea_core';
 import { EP_PHOVEA_CORE_LOCALE } from 'phovea_core';
 //register all extensions in the registry following the given pattern
 export default function (registry) {
-    //registry.push('extension-type', 'extension-id', function() { return import('./src/extension_impl'); }, {});
+    //registry.push('extension-type', 'extension-id', function() { return import('./dist/extension_impl'); }, {});
     registry.push(EP_PHOVEA_CORE_LOCALE, 'phoveaSecurityFlaskLocaleEN', function () {
-        return import('./assets/locales/en/phovea.json').then(PluginRegistry.getInstance().asResource);
+        return import('./dist/assets/locales/en/phovea.json').then(PluginRegistry.getInstance().asResource);
     }, {
         ns: 'phovea',
     });
