d90a23d7960dcdfd9953cbbb3a91de0ca5b1644d
Florian Engertsberger <florian.engertsberger@datavisyn.io>
2021-09-20 15:39:26 +0200

    add data-testid to ordion login menu

--- src/base/LoginMenu.ts
+++ src/base/LoginMenu.ts
@@ -73,6 +73,7 @@ export class LoginMenu extends EventHandler {
   }
 
   private init() {
+    const testId = 'loginmenu';
     const doc = this.options.document;
     const ul = doc.createElement('ul');
     ul.classList.add('navbar-nav', 'navbar-right');
@@ -84,9 +85,9 @@ export class LoginMenu extends EventHandler {
       </li>
       <li style="display: none" class="nav-item dropdown" id="user_menu">
           <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-haspopup="true" id="userMenuDropdown"
-              aria-expanded="false"><i class="fas fa-user" aria-hidden="true"></i> <span>${I18nextManager.getInstance().i18n.t('phovea:security_flask.unknown')}</span></a>
+              aria-expanded="false" data-testid="${testId}-login-dropdown-link"><i class="fas fa-user" aria-hidden="true"></i> <span>${I18nextManager.getInstance().i18n.t('phovea:security_flask.unknown')}</span></a>
           <div class="dropdown-menu dropdown-menu-end" data-bs-popper="none" aria-labelledby="userMenuDropdown">
-              <a class="dropdown-item" href="#" id="logout_link">${I18nextManager.getInstance().i18n.t('phovea:security_flask.logoutButton')}</a>
+              <a class="dropdown-item" href="#" id="logout_link" data-testid="${testId}-logout-link">${I18nextManager.getInstance().i18n.t('phovea:security_flask.logoutButton')}</a>
           </div>
       </li>`;
 
